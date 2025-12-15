87ff24d59f9ce1b54d6d2433f7ca34a0da38b94b
Dharm Patel <dharmpatel311287@gmail.com>
2025-12-15 14:17:28 +0530

    Updated Username

--- moviesite/settings.py
+++ moviesite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7l@45qw)jr8+(p1u_0g=*(*blt&o&j8p#ftf-g@@*m&v@_i@wo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['yourusername.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['Dharm3112.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
