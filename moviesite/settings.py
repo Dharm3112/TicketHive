034cde96f58656804a2d93b4005c9287cc8c25c3
Dharm Patel <dharmpatel311287@gmail.com>
2025-12-15 14:15:30 +0530

    Prepared settings for deployment

--- moviesite/settings.py
+++ moviesite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7l@45qw)jr8+(p1u_0g=*(*blt&o&j8p#ftf-g@@*m&v@_i@wo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
